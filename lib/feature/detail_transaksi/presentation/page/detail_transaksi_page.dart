3af26739d6e7e6644bb83f8702fe4b07f2e09a51
pfnazhmi <pfnazhmi@gmail.com>
2023-12-29 20:22:26 +0700

    update

---
+++ lib/feature/detail_transaksi/presentation/page/detail_transaksi_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fe_rpl/feature/detail_transaksi/presentation/widget/detail_transaksi_component.dart';
+
+class DetailTransaksiPage extends StatelessWidget {
+  const DetailTransaksiPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DetailTransaksiComponent();
+  }
+}
--- lib/feature/transaksi/presentation/widget/card_course_transaksi.dart
+++ lib/feature/transaksi/presentation/widget/card_course_transaksi.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fe_rpl/core/config/app_color.dart';
+import 'package:flutter_fe_rpl/feature/detail_transaksi/presentation/page/detail_transaksi_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CourseCardTransaksi extends StatelessWidget {
@@ -7,79 +8,88 @@ class CourseCardTransaksi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        border: Border.all(
-          color: Color(0xFF7A7A7A).withOpacity(0.2),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetailTransaksiPage(),
+            ));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: Color(0xFF7A7A7A).withOpacity(0.2),
+          ),
         ),
-      ),
-      padding: const EdgeInsets.all(16),
-      child: IntrinsicHeight(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image.network(
-                "https://picsum.photos/200/300",
-                fit: BoxFit.cover,
-                height: 70,
-                width: 90,
+        padding: const EdgeInsets.all(16),
+        child: IntrinsicHeight(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.network(
+                  "https://picsum.photos/200/300",
+                  fit: BoxFit.cover,
+                  height: 70,
+                  width: 90,
+                ),
               ),
-            ),
-            const SizedBox(width: 16),
-            Expanded(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Nama Course",
-                    style: GoogleFonts.poppins(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                      color: AppColor.textPrimary,
-                    ),
-                    overflow: TextOverflow.clip,
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Text(
-                    "RP Harga ",
-                    style: GoogleFonts.poppins(
-                      fontSize: 12,
-                      fontWeight: FontWeight.w500,
-                      color: AppColor.textSecondary,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: AppColor.primary,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 10,
-                      vertical: 2,
-                    ),
-                    child: Text(
-                      "Sukses",
+              const SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Nama Course",
                       style: GoogleFonts.poppins(
-                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
+                        color: AppColor.textPrimary,
+                      ),
+                      overflow: TextOverflow.clip,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      "RP Harga ",
+                      style: GoogleFonts.poppins(
                         fontSize: 12,
                         fontWeight: FontWeight.w500,
+                        color: AppColor.textSecondary,
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: AppColor.primary,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 2,
+                      ),
+                      child: Text(
+                        "Sukses",
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
